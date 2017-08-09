2e058246f2e197b66de7ff1dc99d061938da1319
dart-wakar <wakar@whitelogic.in>
2017-08-09 07:49:28 +0530

    events for comment add and remove

--- src/FbInteractiveComments.jsx
+++ src/FbInteractiveComments.jsx
@@ -13,6 +13,8 @@ export default class FbInteractiveComments extends React.Component {
             fbInitialized: false
         }
         this.finished_rendering = this.finished_rendering.bind(this);
+        this.comment_create = this.comment_create.bind(this);
+        this.comment_remove = this.comment_remove.bind(this);
     }
 
     componentDidMount() {
@@ -24,9 +26,19 @@ export default class FbInteractiveComments extends React.Component {
         console.log('finished_rendering');
     }
 
+    comment_create() {
+        console.log('comment_create');
+    }
+
+    comment_remove() {
+        console.log('comment_remove');
+    }
+
     render() {
         if(this.state.fbInitialized) {
             this.fb.Event.subscribe('xfbml.render',this.finished_rendering);
+            this.fb.Event.subscribe('comment.create',this.comment_create);
+            this.fb.Event.subscribe('comment.remove',this.comment_remove);
             return (
                 <FbComments appId={this.props.appId} apiVersion={this.props.apiVersion} urL={this.props.urL} isNotChild={false}/>
             );
